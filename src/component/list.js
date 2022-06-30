6caf19b9ecc3bbc0f9c76e880637d288cbc37590
pankaj <er.pankaj2021@gmail.com>
2022-06-30 23:43:13 +0530

    final changes

--- src/component/list.js
+++ src/component/list.js
@@ -4,10 +4,8 @@ import Listadd from "./Listadd";
 export default function List() {
   const [inputitem, setInputItem] = useState("");
   const [item, setItem] = useState([]);
-  const[toggleEdit, setToggleEdit]=useState(true)
-  const [editId,setEditId]=useState(null)
-
-
+  const [toggleEdit, setToggleEdit] = useState(true);
+  const [editId, setEditId] = useState(null);
 
   const click = () => {
     if (inputitem.trim().length !== 0) {
@@ -18,25 +16,25 @@ export default function List() {
         };
         return [...item, keyitems];
       });
-    }
-    else if(inputitem && !toggleEdit){
-      setItem(item.map((elem)=>{
-           if(elem.id===editId){
-            return{...elem, name:inputitem}
-           }
-           return elem
-      }
-      
-      ))
-     setToggleEdit(true)
-     setInputItem('')
-     setEditId(null)
-      
-    } 
-    else {
+    } else {
       alert("field required");
     }
     setInputItem(" ");
+  };
+  const click2 = () => {
+    if (inputitem && !toggleEdit) {
+      setItem(
+        item.map((elem) => {
+          if (elem.id === editId) {
+            return { ...elem, name: inputitem };
+          }
+          return elem;
+        })
+      );
+      setToggleEdit(true);
+      setInputItem("");
+      setEditId(null);
+    }
   };
   //delete functionality
   const Deleted = (x) => {
@@ -48,16 +46,21 @@ export default function List() {
   };
   //edit button functionaliy
   const Edit = (Id) => {
-    console.log(Id)
+    console.log(Id);
     // console.log(newEditItem.name)
-     let newEditItem=item.find((elem)=>{
-      return elem.id===Id
-     })
-     setToggleEdit(false)
-     setInputItem(newEditItem.name)
-     setEditId(Id)
-
-
+    let newEditItem = item.find((elem) => {
+      return elem.id === Id;
+    });
+    setToggleEdit(false);
+    setInputItem(newEditItem.name);
+    setEditId(Id);
+  };
+  // clear all functionality
+  const clearall = () => {
+    const data1 = item.filter((val) => {
+      return null;
+    });
+    setItem(data1);
   };
   return (
     <>
@@ -76,11 +79,18 @@ export default function List() {
                 setInputItem(e.target.value);
               }}
             />
-            {toggleEdit?<span class="material-symbols-outlined addbtn"  onClick={click}>add_circle</span>:
-            <span className="material-symbols-outlined edit" onClick={() => {
-            
-          }}>edit</span>}
-            
+            {toggleEdit ? (
+              <span
+                className="material-symbols-outlined addbtn"
+                onClick={click}
+              >
+                add_circle
+              </span>
+            ) : (
+              <span className="material-symbols-outlined edit" onClick={click2}>
+                edit
+              </span>
+            )}
           </div>
 
           {item.map((listitem) => {
@@ -93,6 +103,12 @@ export default function List() {
               />
             );
           })}
+          <div className="clearbtndiv">
+            
+            <span class="material-symbols-outlined" onClick={clearall}>
+check_circle
+</span>
+          </div>
         </div>
       </div>
     </>
